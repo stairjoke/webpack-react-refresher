aac29b40038dc408b817f0e39cc9844ac9caf2d0
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-09-02 13:45:11 +0000

    Added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,17 @@ const path = require("path")
 
 module.exports = {
 	mode:  "development",
-	devtool: "none",
 	entry: "./src/index.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
 	}
 }
